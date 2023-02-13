47eecf736d45a42bb9dae00734124c3f5e279c20
+ <+>
2023-02-13 22:16:16 +0300

    completed assignment

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,13 @@
-class Student 
+class Student < User
+    def initialize
+        @knowledge = []
+    end
+    def learn(knowledge)
+        @knowledge<<knowledge
+        # return "Javascript Ember Elixir knowledge"
+    end
+    def knowledge
+        @knowledge
+    end
 
 end
